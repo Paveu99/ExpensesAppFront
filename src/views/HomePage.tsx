e77c4381bbec608c59eccd72c54c404307078078
Paweł Jarecki <paweljarecki10@gmail.com>
2023-11-28 21:03:19 +0100

    React added with component export to each element

---
+++ src/views/HomePage.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+export const HomePage = () => {
+  return <div>Home Page
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  <p>ej</p>
+  </div>
+}
