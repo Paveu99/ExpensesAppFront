07e926c158772f06567cbe9ad6dca0edfdb9fe90
Paweł Jarecki <paweljarecki10@gmail.com>
2023-11-28 21:06:05 +0100

    Initial design of App.tsx with added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./components/header/Header";
+import {Route, Routes} from "react-router-dom";
+import {HomePage} from "./views/HomePage";
+import {InfoPage} from "./views/InfoPage";
+import {CreatorPage} from "./views/CreatorPage";
+import {DetailedPage} from "./views/DetailedPage";
 export const App = () => {
 
   return (
     <div className="App">
-      <header>
-        Dzień dobry
-      </header>
-      <div>
-        Reszta
-      </div>
+        <Header/>
+        <div className="content">
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/info" element={<InfoPage/>}/>
+                <Route path="/creator" element={<CreatorPage/>}/>
+                <Route path="/detailed/:id" element={<DetailedPage/>}/>
+            </Routes>
+        </div>
     </div>
   );
 }
