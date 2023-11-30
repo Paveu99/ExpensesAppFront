8105d763d8b3bc7ab6051aad2eb2cd23be48fa26
Paweł Jarecki <paweljarecki10@gmail.com>
2023-11-30 20:57:41 +0100

    New functionality of hiding navbar added to the web application - now you can toggle between seeing it or not

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import './App.scss';
 import {Header} from "./components/header/Header";
 import {Route, Routes} from "react-router-dom";
 import {HomePage} from "./views/HomePage";
 import {InfoPage} from "./views/InfoPage";
 import {CreatorPage} from "./views/CreatorPage";
 import {DetailedPage} from "./views/DetailedPage";
+import el from './components/styles/images/Menu.png';
 export const App = () => {
+
+    const [isNavbarHidden, setIsNavbarHidden] = useState<boolean>(false);
+
+    const toggleNavbar = () => {
+        setIsNavbarHidden(!isNavbarHidden);
+    }
 
   return (
     <div className="App">
-        <Header/>
+        <button className="test-button" onClick={toggleNavbar}><img src={el} alt="" className="icon-inside"/></button>
+        <div className={`navbar ${isNavbarHidden ? 'hidden' : ''}`}>
+            <Header/>
+        </div>
         <div className="content">
             <Routes>
                 <Route path="/" element={<HomePage/>}/>
