8681dba6ed14cf69bdf0ce70aa6e9eb68c277301
Paweł Jarecki <paweljarecki10@gmail.com>
2023-11-30 19:50:41 +0100

    Due to getting rid of unwanted icon the numbers of other elements called in this component changed

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import {NavLink} from "react-router-dom"
-import el1 from '../styles/images/Dollar.png';
 import "../styles/Header.scss";
-import el2 from '../styles/images/Home.png';
-import el3 from '../styles/images/Info.png';
-import el4 from '../styles/images/Details.png';
-import el5 from '../styles/images/user.png';
+import el1 from '../styles/images/Home.png';
+import el2 from '../styles/images/Info.png';
+import el3 from '../styles/images/Details.png';
+import el4 from '../styles/images/user.png';
 export const Header = () => {
     const styleOfLink = ({isActive}: {
                              isActive: boolean
@@ -17,22 +16,21 @@ export const Header = () => {
         }
     )
   return <header className="nav">
-      <img src={el1} alt=""/>
       <h1 className="title">EXPENSE APP</h1>
       <NavLink className="nav__link" style={styleOfLink} to="/">
-          <img className="small-icon" src={el2} alt=""/>
+          <img className="small-icon" src={el1} alt=""/>
           <p className="text">Main</p>
       </NavLink>
       <NavLink className="nav__link" style={styleOfLink} to="/info">
-          <img className="small-icon" src={el3} alt=""/>
+          <img className="small-icon" src={el2} alt=""/>
           <p className="text">Information</p>
       </NavLink>
       <NavLink className="nav__link" style={styleOfLink} to="/creator">
-          <img className="small-icon" src={el5} alt=""/>
+          <img className="small-icon" src={el4} alt=""/>
           <p className="text">Creator</p>
       </NavLink>
       <NavLink className="nav__link" style={styleOfLink} to="/details">
-          <img className="small-icon" src={el4} alt=""/>
+          <img className="small-icon" src={el3} alt=""/>
           <p className="text">Details</p>
       </NavLink>
       <button className="add-form-button">+ New expense</button>
