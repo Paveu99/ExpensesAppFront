b3863d5d36e51480ef05fd7c461cb6e7f105b2ee
Paweł Jarecki <paweljarecki10@gmail.com>
2023-12-09 21:11:03 +0100

    Initial state of CreatorPage.tsx

--- src/views/CreatorPage.tsx
+++ src/views/CreatorPage.tsx
@@ -1,5 +1,57 @@
 import React from 'react';
+import '../components/styles/Creator.scss'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import '../components/styles/Creator.scss'
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import {Keyboard, Mousewheel, Pagination} from 'swiper/modules';
+
+interface AnimatedSlideProps {
+    children: React.ReactNode;
+}
+
+const AnimatedSlide = (children: AnimatedSlideProps ) => {
+    const [ref, inView] = useInView({
+        triggerOnce: true,
+    });
+
+    const variants = {
+        hidden: { opacity: 0, y: 50 },
+        visible: { opacity: 1, y: 0 },
+    };
+
+    return (
+        <motion.div
+            ref={ref}
+            initial="hidden"
+            animate={inView ? 'visible' : 'hidden'}
+            variants={variants}
+            transition={{ duration: 2 }}
+        >
+            {children.children}
+        </motion.div>
+    );
+};
 
 export const CreatorPage = () => {
-  return <div>Creator Page</div>
-}
+    return <div className="creator">
+            <Swiper
+                modules={[Mousewheel, Pagination, Keyboard]}
+                direction={'horizontal'}
+                slidesPerView={1}
+                spaceBetween={30}
+                mousewheel={true}
+                keyboard
+                pagination={{
+                    clickable: true,
+                }}
+                className="mySwiper"
+            >
+                <SwiperSlide><AnimatedSlide>Hejka</AnimatedSlide></SwiperSlide>
+                <SwiperSlide><AnimatedSlide>Stulejka</AnimatedSlide></SwiperSlide>
+                <SwiperSlide><AnimatedSlide>E;p</AnimatedSlide></SwiperSlide>
+            </Swiper>
+        </div>
+};
