b2faeb47992279d8359bcacdcab283caa0bbf852
Paweł Jarecki <paweljarecki10@gmail.com>
2023-12-10 17:28:24 +0100

    Similar layout for info component

--- src/views/InfoPage.tsx
+++ src/views/InfoPage.tsx
@@ -1,47 +1,17 @@
 import React from 'react';
-import '../components/styles/Creator.scss'
+// import '../components/styles/Info.scss'
 import { Swiper, SwiperSlide } from 'swiper/react';
+import {Keyboard, Mousewheel, Pagination} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import '../components/styles/Creator.scss'
-import { motion } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
-import {Keyboard, Mousewheel, Pagination} from 'swiper/modules';
-
-interface AnimatedSlideProps {
-  children: React.ReactNode;
-}
-
-const AnimatedSlide = (children: AnimatedSlideProps ) => {
-  const [ref, inView] = useInView({
-    triggerOnce: true,
-  });
-
-  const variants = {
-    hidden: { opacity: 0, y: 50 },
-    visible: { opacity: 1, y: 0 },
-  };
-
-  return (
-      <motion.div
-          ref={ref}
-          initial="hidden"
-          animate={inView ? 'visible' : 'hidden'}
-          variants={variants}
-          transition={{ duration: 2 }}
-      >
-        {children.children}
-      </motion.div>
-  );
-};
+import {AnimatedSlide} from "../components/slides/AnimatedSlide";
 
 export const InfoPage = () => {
   return <div className="creator">
     <Swiper
         modules={[Mousewheel, Pagination, Keyboard]}
-        direction={'horizontal'}
+        direction={'vertical'}
         slidesPerView={1}
-        spaceBetween={30}
         mousewheel={true}
         keyboard
         pagination={{
