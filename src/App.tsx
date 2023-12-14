a49cc8703f22da349a7242ad6ec39cf0c6a626d7
Paweł Jarecki <paweljarecki10@gmail.com>
2023-12-14 21:10:45 +0100

    Wrapping whole app around RecordProvider

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {InfoPage} from "./views/InfoPage";
 import {CreatorPage} from "./views/CreatorPage";
 import {DetailedPage} from "./views/DetailedPage";
 import el from './components/styles/images/Menu.png';
+import {RecordProvider} from "./components/context/RecordContext";
 export const App = () => {
 
     const [isNavbarHidden, setIsNavbarHidden] = useState<boolean>(true);
@@ -17,18 +18,20 @@ export const App = () => {
 
   return (
     <div className="App">
-        <button className="test-button" onClick={toggleNavbar}><img src={el} alt="" className="icon-inside"/></button>
-        <div className={`navbar ${isNavbarHidden ? 'hidden' : ''}`}>
-            <Header/>
-        </div>
-        <div className="content">
-            <Routes>
-                <Route path="/" element={<HomePage/>}/>
-                <Route path="/info" element={<InfoPage/>}/>
-                <Route path="/creator" element={<CreatorPage/>}/>
-                <Route path="/detailed/:id" element={<DetailedPage/>}/>
-            </Routes>
-        </div>
+        <RecordProvider>
+            <button className="test-button" onClick={toggleNavbar}><img src={el} alt="" className="icon-inside"/></button>
+            <div className={`navbar ${isNavbarHidden ? 'hidden' : ''}`}>
+                <Header/>
+            </div>
+            <div className="content">
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="/info" element={<InfoPage/>}/>
+                    <Route path="/creator" element={<CreatorPage/>}/>
+                    <Route path="/details" element={<DetailedPage/>}/>
+                </Routes>
+            </div>
+        </RecordProvider>
     </div>
   );
 }
