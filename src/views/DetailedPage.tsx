2f126372a93cae73c624107d66e1180cb17fb901
Paweł Jarecki <paweljarecki10@gmail.com>
2023-12-28 10:55:26 +0100

    Beta version of years and all time data

--- src/views/DetailedPage.tsx
+++ src/views/DetailedPage.tsx
@@ -1,23 +1,29 @@
 import React, {useEffect} from 'react';
 import {useRecordContext} from "../components/context/RecordContext";
+import '../components/styles/Detailed.scss';
+import el1 from "../components/styles/images/Home.png";
+import {NavLink} from "react-router-dom";
 
 export const DetailedPage = () => {
-  const { records, fetchRecords } = useRecordContext();
+  const { groupedByDate, fetchRecords } = useRecordContext();
 
   useEffect(() => {
     fetchRecords();
   }, []);
 
-  console.log(records)
-
   return (
-      <div className="records">
-        <h2>Record List</h2>
-        <ul>
-          {records.map((record, index) => (
-              <li key={index}>{record.name} {record.id}</li>
-          ))}
-        </ul>
-      </div>
+        <div className="detailed">
+            <h1>Choose a year</h1>
+            <div className="years-to-choose">
+                {Object.keys(groupedByDate).map((year, index) => (
+                    <NavLink className="year-link" to={`/details/${year}`}>
+                        <p className="text">{year}</p>
+                    </NavLink>
+                ))}
+            </div>
+            <div className="overall-stats">
+                Okej tu będą jakieś ogólne statystyki
+            </div>
+        </div>
   );
 }
