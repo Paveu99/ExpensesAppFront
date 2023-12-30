7c45f09b8b9723a1854105df0bba903dca80f726
Paweł Jarecki <paweljarecki10@gmail.com>
2023-12-30 13:45:04 +0100

    Allowing mew Route to exist

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import {DetailedPage} from "./views/DetailedPage";
 import el from './components/styles/images/Menu.png';
 import {RecordProvider} from "./components/context/RecordContext";
 import {DetailedPageYear} from "./views/DetailedPageYear";
+import {DetailedPageMonth} from "./views/DetailedPageMonth";
 export const App = () => {
 
     const [isNavbarHidden, setIsNavbarHidden] = useState<boolean>(true);
@@ -31,7 +32,7 @@ export const App = () => {
                     <Route path="/creator" element={<CreatorPage/>}/>
                     <Route path="/details" element={<DetailedPage/>}/>
                     <Route path="/details/:year" element={<DetailedPageYear/>}/>
-                    {/*<Route path="/details/:year/:month" element={<DetailedPageYear/>}/>*/}
+                    <Route path="/details/:year/:month" element={<DetailedPageMonth/>}/>
                 </Routes>
             </div>
         </RecordProvider>
