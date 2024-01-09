3b6a3d6620edb891e10f9a50cdc0a65f7ec07222
Paweł Jarecki <paweljarecki10@gmail.com>
2024-01-09 23:16:56 +0100

    Creating new component with test values inside of it - soon to be edit form

--- src/components/expenses/DetailedInfo.tsx
+++ src/components/expenses/DetailedInfo.tsx
@@ -1,1 +1,26 @@
-import React from "react";
+import React from 'react';
+import { ExpenseEntity } from 'types';
+import '../styles/DetailedExpense.scss'
+
+interface Props {
+    expense: ExpenseEntity;
+    onClose: () => void;
+}
+
+export const ExpenseDetailsPanel = (props: Props) => {
+    return (
+        <div className="whole-panel">
+            <button className="close-button" onClick={props.onClose}>
+                X
+            </button>
+            <h2>Expense Details</h2>
+            <div className="info-panel">
+                <p>Name: {props.expense.name}</p>
+                <p>Date: {props.expense.month}</p>
+                <p>Cost: {props.expense.cost}$</p>
+                <p>Category: {props.expense.category}</p>
+                <p>Notes: {props.expense.notes}</p>
+            </div>
+        </div>
+    );
+};
