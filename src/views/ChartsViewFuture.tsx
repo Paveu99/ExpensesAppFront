6a17260896970188cfc5b648af3b7ea34e662478
Paweł Jarecki <paweljarecki10@gmail.com>
2024-01-17 11:37:38 +0100

    Future view updated with new charts

--- src/views/ChartsViewFuture.tsx
+++ src/views/ChartsViewFuture.tsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useRecordContext} from "../components/context/RecordContext";
+import {LinearChartFuture} from "../components/charts/LinearChartFuture";
+import {PieChartFuture} from "../components/charts/PieChartFuture";
+import {HistogramChartFuture} from "../components/charts/HistogramChartFuture";
 
 export const ChartsViewFuture = () => {
-    return <h1>Hi from future</h1>
+    const {allFutureRecords, groupedByYearFuture, fetchFutureRecords } = useRecordContext();
+
+    useEffect(() => {
+        fetchFutureRecords();
+    }, []);
+
+    return <>
+        <div className="chart-view-past">
+            <div style={{marginBottom: "100px"}}>
+                <LinearChartFuture expensesGroupedByYear={groupedByYearFuture}/>
+            </div>
+            <div style={{marginBottom: "100px"}}>
+                <PieChartFuture allExpenses={allFutureRecords}/>
+            </div>
+            <div style={{marginBottom: "100px"}}>
+                <HistogramChartFuture data={allFutureRecords}/>
+            </div>
+        </div>
+        {/*<div className="chart-view-past2">*/}
+        {/*    <div style={{marginBottom: "100px"}}>*/}
+        {/*        <HeatMapChartPast data={allRecords}/>*/}
+        {/*    </div>*/}
+        {/*</div>*/}
+    </>
 }
